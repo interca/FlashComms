9a94ff7e7a0eb0b7e4486d0a73f9fb8acdf15e0c
hongsheng <1634340724@qq.com>
2023-10-06 02:23:15 +0800

    编写处理器

--- flashcomms-chat-server/src/main/java/com/im/flashcomms/common/websocket/NettyWebSocketServerHandler.java
+++ flashcomms-chat-server/src/main/java/com/im/flashcomms/common/websocket/NettyWebSocketServerHandler.java
@@ -1,5 +1,6 @@
 package com.im.flashcomms.common.websocket;
 
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
@@ -8,6 +9,8 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
  * 自定义业务处理
  *
  */
+//处理器共用
+@ChannelHandler.Sharable
 public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
