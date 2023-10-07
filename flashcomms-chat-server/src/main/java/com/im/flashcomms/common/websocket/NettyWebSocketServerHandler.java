3b79e05ebfb34d5fe417356f40996f0037652a95
hongsheng <1634340724@qq.com>
2023-10-07 16:01:11 +0800

    添加心跳检查

--- flashcomms-chat-server/src/main/java/com/im/flashcomms/common/websocket/NettyWebSocketServerHandler.java
+++ flashcomms-chat-server/src/main/java/com/im/flashcomms/common/websocket/NettyWebSocketServerHandler.java
@@ -1,5 +1,6 @@
 package com.im.flashcomms.common.websocket;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.im.flashcomms.common.websocket.domain.enums.WSReqTypeEnum;
 import com.im.flashcomms.common.websocket.domain.vo.req.WSBaseReq;
@@ -7,6 +8,9 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 
 /**
  * 自定义业务处理
@@ -15,12 +19,36 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 //处理器共用
 @ChannelHandler.Sharable
 public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+
+    /**
+     * 心跳检查
+     *
+     * @param ctx
+     * @param evt
+     * @throws Exception
+     */
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent idleStateEvent = (IdleStateEvent) evt;
+            // 读空闲
+            if (idleStateEvent.state() == IdleState.READER_IDLE) {
+                // 关闭用户的连接
+                System.out.println("用户下线");
+            }
+        } else if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
+             System.out.println("握手成功");
+        }
+        //super.userEventTriggered(ctx, evt);
+    }
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
         String text = msg.text();
         WSBaseReq wsBaseReq = JSONUtil.toBean(text, WSBaseReq.class);
         switch (WSReqTypeEnum.of(wsBaseReq.getType())){
             case LOGIN:
+                ctx.channel().writeAndFlush(new TextWebSocketFrame("123"));
                 break;
             case AUTHORIZE:
                 break;
