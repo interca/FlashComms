278f05fc644b1df34e02b6136380aec7b6fa7e29
hongsheng <1634340724@qq.com>
2023-10-31 15:29:58 +0800

    引入本地消息模块

---
+++ flashcomms-framework/flashcomms-transaction/src/main/java/com/im/flashcomms/transaction/mapper/SecureInvokeRecordMapper.java
@@ -0,0 +1,8 @@
+package com.im.flashcomms.transaction.mapper;
+
+import com.abin.mallchat.transaction.domain.entity.SecureInvokeRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface SecureInvokeRecordMapper extends BaseMapper<SecureInvokeRecord> {
+
+}
