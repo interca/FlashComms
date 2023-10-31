4f611afd480b8c77ae7d70fa3b88a86a135043c2
hongsheng <1634340724@qq.com>
2023-10-31 23:58:20 +0800

    添加消费者类

--- flashcomms-chat-server/src/main/java/com/im/flashcomms/common/chat/consumer/RabbitMqListener.java
+++ flashcomms-chat-server/src/main/java/com/im/flashcomms/common/chat/consumer/RabbitMqListener.java
@@ -7,6 +7,10 @@ import com.im.flashcomms.common.chat.dao.ContactDao;
 import com.im.flashcomms.common.chat.dao.MessageDao;
 import com.im.flashcomms.common.chat.dao.RoomDao;
 import com.im.flashcomms.common.chat.dao.RoomFriendDao;
+import com.im.flashcomms.common.chat.domain.entity.Room;
+import com.im.flashcomms.common.chat.domain.entity.RoomFriend;
+import com.im.flashcomms.common.chat.domain.enums.RoomTypeEnum;
+import com.im.flashcomms.common.chat.domain.vo.response.ChatMessageResp;
 import com.im.flashcomms.common.chat.service.ChatService;
 import com.im.flashcomms.common.chat.service.WeChatMsgOperationService;
 import com.im.flashcomms.common.chat.service.cache.GroupMemberCache;
@@ -15,6 +19,7 @@ import com.im.flashcomms.common.chat.service.cache.RoomCache;
 import com.im.flashcomms.common.common.domain.dto.MsgSendMessageDTO;
 import com.im.flashcomms.common.user.cache.UserCache;
 import com.im.flashcomms.common.user.service.Impl.PushService;
+import com.im.flashcomms.common.user.service.adapter.WSAdapter;
 import com.im.flashcomms.common.websocket.service.WebSocketService;
 import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
@@ -23,6 +28,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -68,16 +78,40 @@ public class RabbitMqListener {
      */
     @RabbitListener(queues = "flascomms-msg-queue")
     @Transactional
-    public  void robTicket(Message message, Channel channel) throws Exception {
+    public  void robTicket(Message msg, Channel channel) throws Exception {
         String str ;
         try {
             //接受消息
-            str =  new String(message.getBody());
-            MsgSendMessageDTO msgSendMessageDTO = JSONUtil.toBean(str, MsgSendMessageDTO.class);
-            Long msgId = msgSendMessageDTO.getMsgId();
+            str =  new String(msg.getBody());
+            MsgSendMessageDTO dto = JSONUtil.toBean(str, MsgSendMessageDTO.class);
+            com.im.flashcomms.common.chat.domain.entity.Message message = messageDao.getById(dto.getMsgId());
+            Room room = roomCache.get(message.getRoomId());
+            ChatMessageResp msgResp = chatService.getMsgResp(message, null);
+            //所有房间更新房间最新消息
+            roomDao.refreshActiveTime(room.getId(), message.getId(), message.getCreateTime());
+            roomCache.delete(room.getId());
+            if (room.isHotRoom()) {//热门群聊推送所有在线的人
+                //更新热门群聊时间-redis
+                hotRoomCache.refreshActiveTime(room.getId(), message.getCreateTime());
+                //推送所有人
+                pushService.sendPushMsg(WSAdapter.buildMsgSend(msgResp));
+            } else {
+                List<Long> memberUidList = new ArrayList<>();
+                if (Objects.equals(room.getType(), RoomTypeEnum.GROUP.getType())) {//普通群聊推送所有群成员
+                    memberUidList = groupMemberCache.getMemberUidList(room.getId());
+                } else if (Objects.equals(room.getType(), RoomTypeEnum.FRIEND.getType())) {//单聊对象
+                    //对单人推送
+                    RoomFriend roomFriend = roomFriendDao.getByRoomId(room.getId());
+                    memberUidList = Arrays.asList(roomFriend.getUid1(), roomFriend.getUid2());
+                }
+                //更新所有群成员的会话时间
+                contactDao.refreshOrCreateActiveTime(room.getId(), memberUidList, message.getId(), message.getCreateTime());
+                //推送房间成员
+                pushService.sendPushMsg(WSAdapter.buildMsgSend(msgResp), memberUidList);
+            }
         } catch (Exception e) {
             //如果发送异常  消息重新回队列
-            channel.basicNack(message.getMessageProperties().getDeliveryTag(),true,true);
+            channel.basicNack(msg.getMessageProperties().getDeliveryTag(),true,true);
             throw new RuntimeException(e);
         }
     }
